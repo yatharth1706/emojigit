78b2d70f6c42e754887f82c78f2a6de4dd1cc0d3
yatharth <yatharthverma070@gmail.com>
2020-06-08 19:53:39 +0530

    :hammer: Made basic cli setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-const inquirer = require('inquirer');
+#! /usr/bin/env node
 
-inquirer
-  .prompt([
-    {
-        type: 'confirm',
-        name: 'isHuman',
-        message: 'Are you a human?',
-        default: false
-      }
-  ])
-  .then(answers => {
-    console.log(answers);
-  });
+const fs = require('fs');
+const fetch = require("node-fetch");
+const inquirer = require('inquirer');
+const ora = require("ora");
+const spinner = ora("loading emojis").start();
+
+fetch("https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json").then((res) => {
+        return res.json();
+    }).then((data) => {
+        let emojis = data.gitmojis;
+        let newEmojis = [];
+        emojis.forEach(element => {
+            newEmojis.push({
+                name : `${element.name} - ${element.emoji}`
+            })
+        });
+        setTimeout(() => {
+            spinner.color = 'yellow';
+            spinner.text = 'Loading Emojis';
+        }, 1000);
+        inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'listEmoji',
+                    message: 'Select emoji that you want to use: ',
+                    choices: newEmojis
+                },
+            ])
+            .then((answers) => {
+                console.log(answers);
+                
+            });
+    })
+
+
+
+
