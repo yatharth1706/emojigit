cc574428137084931dbdadb54de161db7720642f
yatharth <yatharthverma070@gmail.com>
2020-06-10 12:53:53 +0530

    :zap: - Improve list command

--- src/commands/list/index.js
+++ src/commands/list/index.js
@@ -1,15 +1,23 @@
 const fetch = require('node-fetch');
 const inquirer = require('inquirer');
+const chalk = require('chalk');
+const ora = require('ora');
+
+const emojisSpinner = ora({
+    text: "Loading Emojis!!"
+});
 
 const list = () => {
+    emojisSpinner.start();
     fetch('https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json')
     .then((res) => res.json())
     .then((data) => {
+        emojisSpinner.succeed(chalk.magenta("Successfully Loaded!!"));
         let tempEmojis = data.gitmojis;
         let emojis = [];
         tempEmojis.forEach((el) => {
             emojis.push({
-                "name" : `${el.emoji} - ${el.description}`
+                "name" : `${el.emoji} - ${el.description} - ${el.code}`
             })
         });
 
@@ -24,7 +32,7 @@ const list = () => {
                 }
             ]
         ).then((answers) => {
-            console.log(answers);
+            console.log(chalk.white.inverse("Github Code for chosen emoji is:"),answers.chooseEmoji.split("-")[2]);
         })
     })
 }
